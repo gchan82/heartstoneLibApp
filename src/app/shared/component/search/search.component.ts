a4b177a69381cc152dd1d5e9d2b5555d0e57f424
gchan82 <42255277+gchan82@users.noreply.github.com>
2018-12-02 12:04:29 -0800

    albumsScreen.js line 33 comments issue

--- src/app/shared/component/search/search.component.ts
+++ src/app/shared/component/search/search.component.ts
@@ -1,26 +1,27 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
 
-export class SearchComponent{
-@Input() items: any[] = [];
+export class SearchComponent {
+  @Input() items: any[] = [];
+  @Input() filteredProperty: string;
 
-@Output() searchCompleted = new EventEmitter();
+  @Output() searchCompleted = new EventEmitter();
 
-handleSearch(event: any){
+  handleSearch(event: any) {
 
-  const searchedText = event.target.value;
+    const searchedText = event.target.value;
 
-  if(!this.items) return this.searchCompleted.emit([]);
-  if(!searchedText) return this.searchCompleted.emit(this.items);
+    if (!this.items) return this.searchCompleted.emit([]);
+    if (!searchedText) return this.searchCompleted.emit(this.items);
 
-  const filteredItems = this.items.filter((item) => {
-    return item.name.toLowerCase().includes(searchedText.toLowerCase());
-  })
+    const filteredItems = this.items.filter((item) => {
+      return item[this.filteredProperty].toLowerCase().includes(searchedText.toLowerCase());
+    });
 
-  this.searchCompleted.emit(filteredItems);
-}
+    this.searchCompleted.emit(filteredItems);
+  }
 }
